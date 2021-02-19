ef4aa83ea0d5ee5359227b1029bbbf231f1978b5
omneya <omneyaosama89@gmail.com>
2021-02-19 03:34:28 +0200

    m

--- Admin_DB/app/Http/Controllers/HomeController.php
+++ Admin_DB/app/Http/Controllers/HomeController.php
@@ -25,4 +25,13 @@ class HomeController extends Controller
     {
         return view('home');
     }
+       
+    public function test()
+    {
+
+        return view('testhome');
+    }
+
+
+
 }
---
+++ Admin_DB/resources/views/testhome.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.admin')
+
+@section('content')
+<p>
+
+this is test home page
+
+
+</p>
+
+@endsection
