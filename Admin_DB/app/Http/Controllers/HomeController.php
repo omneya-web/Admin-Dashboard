6d69a26d00af8e56f8ce2ad7c332ad111b6dc594
youssef97hesham <48891247+youssef97hesham@users.noreply.github.com>
2021-02-20 15:24:10 +0200

    ui structure

--- Admin_DB/app/Http/Controllers/HomeController.php
+++ Admin_DB/app/Http/Controllers/HomeController.php
@@ -13,7 +13,7 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        //$this->middleware('auth');
     }
 
     /**
@@ -32,6 +32,10 @@ class HomeController extends Controller
         return view('testhome');
     }
 
+    public function profile()
+    {
 
+        return view('adminprofile');
+    }
 
 }
