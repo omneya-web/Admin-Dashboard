2241cdf9d7c1c5486200463ad4e6bde127858aaf
maielqady <62443857+maielqady@users.noreply.github.com>
2021-02-23 03:49:17 +0200

    mai

---
+++ Admin_DB/storage/framework/views/7e222bda88c734e417a7bf94e628d7eb2a781773.php
@@ -0,0 +1,24 @@
+
+
+<?php $__env->startSection('content'); ?>
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>Feedback</h1>
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active">Feedback</li>
+            </ol>
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+
+
+<?php echo $__env->make('layouts.includes', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\Admin-Dashboard\Admin_DB\resources\views/Feedback.blade.php ENDPATH**/ ?>
