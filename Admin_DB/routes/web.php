30235a876ee920159d0af1a4352830228392cae6
HossamHassanES <48808115+HossamHassanES@users.noreply.github.com>
2021-05-02 16:37:20 +0200

    Add files via upload

--- Admin_DB/routes/web.php
+++ Admin_DB/routes/web.php
@@ -12,29 +12,36 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-//Route::get('/login', 'LoginController@__construct')->name('home');
-
 Auth::routes();
+Route::post('/login', 'Auth\LoginController@loginAdmin')->name('loginAdmin');
+Route::group(['middleware', 'Admin'], function () {
+
+    Route::get('/dashboard/addUser', 'userController@addUserForm' );
+    Route::post('/dashboard/addUser', 'userController@addUser')->name('addUser');
+    Route::get('/dashboard/showUser', 'userController@showUser');
+    Route::post('/dashboard/editUser', 'userController@updateUser')->name('updateUser');
+    Route::get('/deleteAdmin/{id}', 'AdminController@deleteAdmin');
+    Route::post('/addAdminUser', 'AdminController@addAdminUser')->name('addAdminUser');
+    Route::get('/admins', 'AdminController@getAllAdmin');
+    
+    });
+
 
 Route::get('/home', 'HomeController@index')->name('home');
-
+Route::get('/', 'HomeController@index');
 Route::get('/adminprofile', 'HomeController@profile');
 Route::get('/primary', 'HomeController@primary');
-Route::get('/users', 'HomeController@Users');
+Route::get('/users', 'userController@Users');
 
 
 
-Route::get('/admins', 'HomeController@Admins');
 
-Route::get('/addUser', 'HomeController@addUser');
-Route::get('/addAdmin', 'HomeController@addAdmin');
-Route::get('/userDetails', 'UserController@userDetails');
-Route::get('/medical', 'UserController@showMedical');
+
+Route::get('/addUser', 'HomeController@addUser')->name('addUser');
+Route::get('/addAdmin', 'AdminController@addAdmin');
+Route::post('/addAdmin', 'AdminController@addAdminUser')->name('addAdmin');
+Route::get('/userDetails', 'userController@userDetails');
+Route::get('/medical', 'userController@showMedical');
 Route::get('/adminDetails', 'AdminController@adminDetails');
 Route::get('/feedback', 'HomeController@feedback');
 Route::get('/places', 'HomeController@places');
