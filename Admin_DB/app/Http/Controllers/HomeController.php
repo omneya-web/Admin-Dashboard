b8f43035cce8d07ad22292bbabb462ffcb0c8745
HossamHassanES <48808115+HossamHassanES@users.noreply.github.com>
2021-07-06 19:55:43 +0200

    Add files via upload

--- Admin_DB/app/Http/Controllers/HomeController.php
+++ Admin_DB/app/Http/Controllers/HomeController.php
@@ -1,7 +1,10 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Kreait\Firebase\Factory;
+use Kreait\Firebase\ServiceAccount;
+use Kreait\Firebase\Database;
+use Google\Cloud\Firestore\FirestoreClient;
 use Illuminate\Http\Request;
 use Auth;
 
@@ -29,7 +32,28 @@ class HomeController extends Controller
              Auth::logout();
             return redirect('/login');
         }
-        return view('home');
+        $users_cnt  = 0;
+        $admins_cnt = 0;
+          
+      $databaseinstance_admin = app('firebase.firestore')->database()->collection('users');
+      $documents_admin = $databaseinstance_admin -> documents();
+       foreach($documents_admin as $document)
+       {
+        if ($document->exists())
+        {
+           $users_cnt++;
+        }
+       } 
+      $databaseinstance_user = app('firebase.firestore')->database()->collection('Admin');
+      $documents_user = $databaseinstance_user -> documents();
+       foreach($documents_user as $document)
+       {
+        if ($document->exists())
+        {
+            $admins_cnt++;
+        }
+       } 
+        return view('home',['users_cnt' => $users_cnt],['admins_cnt' => $admins_cnt]);
     }
        
    
